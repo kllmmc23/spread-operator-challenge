9c43e870f97fc9f9319b839e86f8c83d5dd19c85
Michael <63890254+kllmmc23@users.noreply.github.com>
2020-05-19 22:55:28 -0600

    js-es6-spread-operator-challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    setTextInput(event.target.value);
+  }
+  function handleClick() {
+    setItems((prevItems) => [...prevItems, textInput]);
+    setTextInput("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={textInput} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((lItems) => (
+            <li>{lItems}</li>
+          ))}
         </ul>
       </div>
     </div>
